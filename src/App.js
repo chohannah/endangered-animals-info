36490cb8914c82aab5e0fe2deabc2240bb3db67c
Jinnie Cho <yejincho1105@gmail.com>
2022-07-04 18:37:59 +0900

    ✨ Create list of animals and searchField

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world :)</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { animals: [], searchField: "" };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { animals: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+      .catch((err) => console.error(err));
+  }
+
+  onFieldChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    console.log("rendered!!");
+
+    const filteredAnimals = this.state.animals.filter((animal) => {
+      return animal.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-bar"
+          type="search"
+          placeholder="search here"
+          onChange={this.onFieldChange}
+        />
+        {filteredAnimals.map((animal) => {
+          return (
+            <article key={animal.id}>
+              <h2>{animal.name}</h2>
+            </article>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
