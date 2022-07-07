5293f04ba1e2b4fb43b4dcecd5b72db5fa190233
Jinnie Cho <yejincho1105@gmail.com>
2022-07-08 02:32:00 +0900

    🎨 Modify CardGroup component as a functionalComponent

--- src/components/card-group/card-group.component.jsx
+++ src/components/card-group/card-group.component.jsx
@@ -1,18 +1,13 @@
-import { Component } from "react";
 import Card from "../../card/card.component";
 
-class CardGroup extends Component {
-  render() {
-    const { animals } = this.props;
-
-    return (
-      <main className="card-group">
-        {animals.map((animal) => {
-          return <Card key={animal.id} animal={animal} />;
-        })}
-      </main>
-    );
-  }
-}
+const CardGroup = ({ animals }) => {
+  return (
+    <main className="card-group">
+      {animals.map((animal) => {
+        return <Card key={animal.id} animal={animal} />;
+      })}
+    </main>
+  );
+};
 
 export default CardGroup;
