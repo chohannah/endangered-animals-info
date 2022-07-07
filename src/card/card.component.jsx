4fefe34497817eba4babb546f7a8869b106b13ce
Jinnie Cho <yejincho1105@gmail.com>
2022-07-08 02:40:31 +0900

    🎨 Modify Card component as functionalComponent

--- src/card/card.component.jsx
+++ src/card/card.component.jsx
@@ -1,28 +1,26 @@
-import { Component } from "react";
+const Card = ({ animal }) => {
+  const {
+    image,
+    conservation_status,
+    name: { common, scientific },
+  } = animal;
+  const urlAnimalName = common.toLocaleLowerCase().replaceAll(" ", "-");
 
-export default class Card extends Component {
-  render() {
-    const {
-      image,
-      conservation_status,
-      name: { common, scientific },
-    } = this.props.animal;
-    const urlAnimalName = common.toLocaleLowerCase().replaceAll(" ", "-");
+  return (
+    <article className="card">
+      <a
+        href={`https://www.worldwildlife.org/species/${urlAnimalName}`}
+        alt={`Click to learn more about ${common}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={image} alt={`${common}`} />
+        <h2>{common}</h2>
+        <p>{scientific}</p>
+        <strong>{conservation_status}</strong>
+      </a>
+    </article>
+  );
+};
 
-    return (
-      <article className="card">
-        <a
-          href={`https://www.worldwildlife.org/species/${urlAnimalName}`}
-          alt={`Click to learn more about ${common}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={image} alt={`${common}`} />
-          <h2>{common}</h2>
-          <p>{scientific}</p>
-          <strong>{conservation_status}</strong>
-        </a>
-      </article>
-    );
-  }
-}
+export default Card;
