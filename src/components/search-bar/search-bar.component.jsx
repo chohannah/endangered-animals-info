47fcd1eeadcd913050218eab8fcacbd83f0b8e7e
Jinnie Cho <yejincho1105@gmail.com>
2022-07-08 02:40:49 +0900

    🎨 Modify SearchBar component as functionalComponent

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,19 +1,14 @@
-import { Component } from "react";
-
-class SearchBar extends Component {
-  render() {
-    const { placeholder, onChangeHandler } = this.props;
-    return (
-      <div className="search-bar">
-        <input
-          className="search-bar"
-          type="search"
-          placeholder={placeholder}
-          onChange={onChangeHandler}
-        />
-      </div>
-    );
-  }
-}
+const SearchBar = ({ placeholder, onChangeHandler }) => {
+  return (
+    <div className="search-bar">
+      <input
+        className="search-bar"
+        type="search"
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
