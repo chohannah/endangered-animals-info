ea7c7b2958940d19fd22411004199941bad6a736
Jinnie Cho <yejincho1105@gmail.com>
2022-07-08 00:37:16 +0900

    🎨 Modify App.js from classComponent to functionalComponent

--- src/App.js
+++ src/App.js
@@ -1,48 +1,38 @@
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./components/search-bar/search-bar.component";
 import CardGroup from "./components/card-group/card-group.component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = { animals: [], searchField: "" };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [animals, setAnimals] = useState([]);
+  const [filteredAnimals, setfilteredAnimals] = useState(animals);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://f21b4e44-00af-416a-a8bf-ccb499e75957.mock.pstmn.io")
       .then((response) => response.json())
-      .then((animals) =>
-        this.setState(() => {
-          return { animals };
-        })
-      )
-      .catch((err) => console.error(err));
-  }
+      .then((animals) => setAnimals(animals));
+  }, []);
 
-  onFieldChange = (e) => {
-    const searchField = e.target.value.toLocaleLowerCase();
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { animals, searchField } = this.state;
-    const { onFieldChange } = this;
-
-    const filteredAnimals = animals.filter((animal) => {
+  useEffect(() => {
+    const newFilteredAnimals = animals.filter((animal) => {
       return animal.name.common.toLocaleLowerCase().includes(searchField);
     });
 
-    return (
-      <div className="App">
-        <SearchBar placeholder="search here" onChangeHandler={onFieldChange} />
-        <CardGroup animals={filteredAnimals} />
-      </div>
-    );
-  }
-}
+    setfilteredAnimals(newFilteredAnimals);
+  }, [animals, searchField]);
+
+  const onFieldChange = (e) => {
+    const searchFieldText = e.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldText);
+  };
+
+  return (
+    <div className="App">
+      <SearchBar placeholder="search here" onChangeHandler={onFieldChange} />
+      <CardGroup animals={filteredAnimals} />
+    </div>
+  );
+};
 
 export default App;
