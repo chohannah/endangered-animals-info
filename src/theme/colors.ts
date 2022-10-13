e05830a25f4b4bc7f4e74e7466291be606e7e428
Jinnie Cho <yejincho1105@gmail.com>
2022-10-14 00:41:46 +0900

    💄 Implement colors to theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,29 @@
+const colors = {
+  red: "#ee3350",
+  redOrange: "#f36c42",
+  orange: "#f89834",
+  yellowOrange: "#fec01e",
+  yellow: "#ffe600",
+  yellowGreen: "#cadb3c",
+  green: "#74c050",
+  blueGreen: "#00b9ad",
+  blue: "#0094d5",
+  blueViolet: "#4e5eaa",
+  violet: "#92449a",
+
+  black: "#222",
+  white: "#fff",
+
+  gray: {
+    100: "#efeff0",
+    200: "#e2e3e4",
+    300: "#d5d7d8",
+    400: "#c9cacc",
+    500: "#bcbec0",
+    600: "#afb1b4",
+    700: "#a3a5a8",
+    800: "#97999c",
+  },
+};
+
+export default colors;
