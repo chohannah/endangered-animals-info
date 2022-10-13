b26249e4efb111b2857ca1e304a78f3f3d98b0d2
Jinnie Cho <yejincho1105@gmail.com>
2022-10-14 00:43:18 +0900

    💄 Implement fonts to theme

---
+++ src/theme/fonts.ts
@@ -0,0 +1,8 @@
+import { theme as base } from "@chakra-ui/react";
+
+const fonts = {
+  heading: `"WWF", sans-serif ${base.fonts?.heading}`,
+  body: `"Georgia", serif ${base.fonts?.body}`,
+};
+
+export default fonts;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,8 +2,10 @@ import { extendTheme } from "@chakra-ui/react";
 
 import colors from "./colors";
 import config from "./config";
+import fonts from "./fonts";
 
 export const theme = extendTheme({
   colors,
   config,
+  fonts,
 });
