da4ef9c7f31a9a0c144677d1c61fee5e75a44810
Jinnie Cho <yejincho1105@gmail.com>
2022-10-19 03:10:40 +0900

    🔧 Haneld webkit-search-cancel-button pseudo element in styles theme

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,11 +1,26 @@
-import { mode } from "@chakra-ui/theme-tools";
+import { mode } from '@chakra-ui/theme-tools'
+import colors from './colors'
 
 const styles = {
   global: {
+    html: {
+      color: colors.black,
+    },
     body: {
-      bg: mode("#f0e7db", "#202023"),
+      color: colors.black,
+      bg: mode('#f0e7db', '#202023'),
+    },
+    '::-webkit-search-cancel-button': {
+      '-webkit-appearance': 'none',
+      height: '1em',
+      width: '1em',
+      opacity: '0.6',
+      color: '#222',
+      background:
+        'url(https://raw.githubusercontent.com/feathericons/feather/master/icons/x.svg) no-repeat 50% 50%',
+      'background-size': 'contain',
     },
   },
-};
+}
 
-export default styles;
+export default styles
