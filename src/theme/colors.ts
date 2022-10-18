79cd6c069ab9ed270ae0a591b2d37a13bda451d6
Jinnie Cho <yejincho1105@gmail.com>
2022-10-19 03:15:54 +0900

    💄 Add color theme values: redOrange(300, 600, 900), yellowGreen(400, 900)

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,29 +1,36 @@
 const colors = {
-  red: "#ee3350",
-  redOrange: "#f36c42",
-  orange: "#f89834",
-  yellowOrange: "#fec01e",
-  yellow: "#ffe600",
-  yellowGreen: "#cadb3c",
-  green: "#74c050",
-  blueGreen: "#00b9ad",
-  blue: "#0094d5",
-  blueViolet: "#4e5eaa",
-  violet: "#92449a",
+  red: '#ee3350',
+  redOrange: {
+    300: '#f9b6a1',
+    600: '#f7987b',
+    900: '#f36c42',
+  },
+  orange: '#f89834',
+  yellowOrange: '#fec01e',
+  yellow: '#ffe600',
+  yellowGreen: {
+    400: '#eaf1b1',
+    900: '#cadb3c',
+  },
+  green: '#74c050',
+  blueGreen: '#00b9ad',
+  blue: '#0094d5',
+  blueViolet: '#4e5eaa',
+  violet: '#92449a',
 
-  black: "#222",
-  white: "#fff",
+  black: '#222',
+  white: '#fff',
 
   gray: {
-    100: "#efeff0",
-    200: "#e2e3e4",
-    300: "#d5d7d8",
-    400: "#c9cacc",
-    500: "#bcbec0",
-    600: "#afb1b4",
-    700: "#a3a5a8",
-    800: "#97999c",
+    100: '#efeff0',
+    200: '#e2e3e4',
+    300: '#d5d7d8',
+    400: '#c9cacc',
+    500: '#bcbec0',
+    600: '#afb1b4',
+    700: '#a3a5a8',
+    800: '#97999c',
   },
-};
+}
 
-export default colors;
+export default colors
