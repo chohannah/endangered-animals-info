e406a6906992895a01c4b0993a3032835ae2840c
Jinnie Cho <yejincho1105@gmail.com>
2022-10-19 01:04:34 +0900

    🔨  Add breakpoints in theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,13 +1,15 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
-import styles from "./styles";
-import colors from "./colors";
-import config from "./config";
-import fonts from "./fonts";
+import styles from './styles'
+import colors from './colors'
+import config from './config'
+import fonts from './fonts'
+import breakpoints from './breakpoints'
 
 export const theme = extendTheme({
   colors,
   config,
   fonts,
   styles,
-});
+  breakpoints,
+})
