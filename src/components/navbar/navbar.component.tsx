54d4608f4e41c01a2502db8329613a620ce10d29
Jinnie Cho <yejincho1105@gmail.com>
2022-10-23 01:55:51 +0900

    ✨ Create Navbar component

---
+++ src/components/navbar/navbar.component.tsx
@@ -0,0 +1,86 @@
+import { ReactNode } from 'react'
+import {
+  Box,
+  Flex,
+  Button,
+  Link,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react'
+import ThemeToggleButton from '../theme-toggle-button/theme-toggle-button'
+
+import { NAV_HEIGHT } from '../../utils/style.utils'
+
+const links = {
+  donate:
+    'https://support.worldwildlife.org/site/SPageServer?pagename=main_monthly&s_src=AWE2209OQ18299A01179RX&s_subsrc=topnav',
+  adopt:
+    'https://gifts.worldwildlife.org/gift-center/gifts/Species-Adoptions.aspx',
+}
+
+const NavLink = ({
+  href,
+  backgroundColor,
+  children,
+}: {
+  href: string
+  color: string
+  backgroundColor: string
+  children: ReactNode
+}) => (
+  <Button
+    size={{ base: 'md', md: 'lg' }}
+    variant="solid"
+    backgroundColor={backgroundColor}
+  >
+    <Link
+      href={href}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      h="100%"
+      px="2"
+      py="1"
+      fontFamily="heading"
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+    >
+      {children}
+    </Link>
+  </Button>
+)
+
+const Navbar = () => {
+  return (
+    <Box
+      as="nav"
+      position="fixed"
+      left="0"
+      zIndex="100"
+      px="4"
+      w="100vw"
+      bg={useColorModeValue('#efeff070', '#22222280')}
+    >
+      <Flex h={NAV_HEIGHT} alignItems="center" justifyContent="space-between">
+        <Box>Logo</Box>
+
+        <Flex alignItems="center" gap="2">
+          <Stack direction="row" spacing="2">
+            <NavLink href={links.donate} backgroundColor="red" color="white">
+              Donate
+            </NavLink>
+            <NavLink href={links.adopt} backgroundColor="orange" color="white">
+              Adopt
+            </NavLink>
+          </Stack>
+
+          <ThemeToggleButton />
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Navbar
