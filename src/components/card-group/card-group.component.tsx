6dd20453edd1f282ce793418eeccab197484d379
Jinnie Cho <yejincho1105@gmail.com>
2022-10-24 00:55:52 +0900

    💄 Styled CardGroup component

--- src/components/card-group/card-group.component.tsx
+++ src/components/card-group/card-group.component.tsx
@@ -1,19 +1,37 @@
-import { Animal } from "../../types/animal";
+import { Flex, SimpleGrid } from '@chakra-ui/react'
 
-import Card from "../../card/card.component";
+import { Animal } from '../../types/animal'
+
+import { NAV_HEIGHT } from '../../utils/style.utils'
+
+import Card from '../card/card.component'
 
 type CardGroupProps = {
-  animals: Animal[];
-};
+  animals: Animal[]
+}
 
 const CardGroup = ({ animals }: CardGroupProps) => {
   return (
-    <main className="card-group">
-      {animals.map((animal) => {
-        return <Card key={animal.id} animal={animal} />;
-      })}
-    </main>
-  );
-};
+    <Flex
+      flexDirection="column"
+      justifyContent="start"
+      alignItems="center"
+      mb={{ base: NAV_HEIGHT, md: NAV_HEIGHT + 16 }}
+      w="100%"
+    >
+      <SimpleGrid
+        as="section"
+        columns={{ base: 1, md: 2, lg: 3 }}
+        spacingY="27"
+        spacingX={{ base: 0, md: 27 }}
+        w="100%"
+      >
+        {animals.map((animal) => {
+          return <Card key={animal.id} animal={animal} />
+        })}
+      </SimpleGrid>
+    </Flex>
+  )
+}
 
-export default CardGroup;
+export default CardGroup
