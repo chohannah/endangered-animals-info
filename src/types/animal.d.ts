ed0c033f9cec2c5361e6ef3c2710138cc78688b3
Jinnie Cho <yejincho1105@gmail.com>
2022-10-30 02:34:50 +0900

    🏷️ Update type of link_to_adoption from fetched data

--- src/types/animal.d.ts
+++ src/types/animal.d.ts
@@ -1,11 +1,12 @@
 export interface Name {
-  common: string;
-  scientific: string;
+  common: string
+  scientific: string
 }
 
 export interface Animal extends Name {
-  id: number;
-  name: Name;
-  image: string;
-  conservation_status: string;
+  id: number
+  name: Name
+  image: string
+  conservation_status: string
+  link_to_adoption: string
 }
