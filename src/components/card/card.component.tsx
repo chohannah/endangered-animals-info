6e0e956191a4ea64d95045e1d6610350264f4c4b
Jinnie Cho <yejincho1105@gmail.com>
2022-10-31 20:41:42 +0900

    💄 Modify style of Card component

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,9 +1,9 @@
 import {
   ButtonGroup,
-  Box,
   Flex,
   Link,
   Button,
+  Image,
   Heading,
   Text,
   useColorModeValue,
@@ -48,113 +48,113 @@ const Card = ({ animal }: CardProps) => {
   }
 
   return (
-    <Box
+    <Flex
+      as="article"
       aria-label={`More details of ${common}`}
+      direction="column"
       w="100%"
-      h={{ base: 'md', md: 'xs' }}
+      h={{ base: '80', md: 'sm' }}
       borderRadius="lg"
-      overflow="hidden"
-      backgroundImage={image}
-      backgroundRepeat="none"
-      backgroundSize="cover"
-      backgroundPosition="center"
       _hover={{ textDecoration: 'none' }}
     >
-      <Flex
-        as="article"
-        flexDirection="column"
-        justifyContent="end"
+      <Image
+        src={image}
+        alt={`A photo of ${common}`}
         w="100%"
-        h="100%"
+        h="60%"
+        borderTopRadius="lg"
+        objectFit="cover"
+      />
+
+      <Flex
+        flexDirection="column"
+        justifyContent="space-between"
+        flexGrow="1"
+        p="4"
+        borderBottomRadius="lg"
+        background={useColorModeValue("rgba(239, 239, 240, 0.5)","rgba(255, 255, 255, 0.2)")}
+        backdropFilter="auto"
+        backdropBlur="12px"
       >
-        <Flex
-          flexDirection="column"
-          justifyContent="space-between"
-          flexShrink="1"
-          p="4"
-          backdropFilter="auto"
-          backdropBlur="24px"
-        >
-          <Flex justifyContent="space-between">
-            <Heading
-              as="h3"
-              size={{ base: 'sm', md: 'md' }}
-              fontFamily={fonts.body}
-              fontWeight="black"
-              letterSpacing="tighter"
-              textTransform="uppercase"
-              color={useColorModeValue('black', 'white')}
-              _hover={{ as: 'mark' }}
-            >
-              {common}
-            </Heading>
-
-            <Button
-              variant="solid"
-              size="xs"
-              px="2"
-              color={statusTextColor(conservation_status)}
-              backgroundColor={statusBgColor(conservation_status)}
-              pointerEvents="none"
-              _hover={{ textDecoration: 'none' }}
-            >
-              <WarningTwoIcon mr="1" />
-              {conservation_status}
-            </Button>
-          </Flex>
-
-          <Text
-            mb={{ base: '4' }}
-            size={{ base: 'xs', md: 'sm' }}
-            fontFamily={fonts.mono}
-            fontStyle="italic"
-            color={useColorModeValue('#22222290', '#ffffff90')} // black
+        <Flex justifyContent="space-between">
+          <Heading
+            as="h3"
+            size={{ base: 'sm', md: 'md' }}
+            fontFamily={fonts.body}
+            fontWeight="black"
+            letterSpacing="tighter"
+            textTransform="uppercase"
+            color={useColorModeValue('black', 'gray.100')}
+            _hover={{ as: 'mark' }}
           >
-            {scientific}
-          </Text>
+            {common}
+          </Heading>
 
-          <ButtonGroup size="md">
-            <Button
-              variant="solid"
-              w={{ base: '100%', md: '50%' }}
-              backgroundColor={useColorModeValue('#ffffff20', '#22222270')} // white, black
-              color="yellowOrange"
-              _hover={{
-                backgroundColor: useColorModeValue('#ffffff40', '#22222290'),
-              }}
-            >
-              <Link
-                href={`https://www.worldwildlife.org/species/${urlToAnimal}`}
-                isExternal
-                fontFamily={fonts.heading}
-                fontSize={{ base: 'md', md: 'lg' }}
-                _hover={{ textTransform: 'none' }}
-              >
-                Learn more
-              </Link>
-            </Button>
-            <Button
-              w={{ base: '100%', md: '50%' }}
-              backgroundColor="yellowOrange"
-              color="black"
-              _hover={{
-                backgroundColor: '#fec01e80', // yellowOrange
-              }}
-            >
-              <Link
-                href={link_to_adoption}
-                isExternal
-                fontFamily={fonts.heading}
-                fontSize={{ base: 'md', md: 'lg' }}
-                _hover={{ textTransform: 'none' }}
-              >
-                Adopt
-              </Link>
-            </Button>
-          </ButtonGroup>
+          <Button
+            variant="solid"
+            size="xs"
+            px="2"
+            color={statusTextColor(conservation_status)}
+            backgroundColor={statusBgColor(conservation_status)}
+            pointerEvents="none"
+            _hover={{ textDecoration: 'none' }}
+          >
+            <WarningTwoIcon mr="1" />
+            {conservation_status}
+          </Button>
         </Flex>
+
+        <Text
+          mb={{ base: '4' }}
+          size={{ base: 'xs', md: 'sm' }}
+          fontFamily={fonts.mono}
+          fontStyle="italic"
+          color={useColorModeValue('#22222290', '#ffffff90')} // black
+        >
+          {scientific}
+        </Text>
+
+        <ButtonGroup size={{ base: 'md', lg: 'lg'}}>
+          <Button
+            variant="solid"
+            w={{ base: '100%', md: '50%' }}
+            backgroundColor={useColorModeValue('#ffffff20', '#22222270')} // white, black
+            color="yellowOrange"
+            _hover={{
+              backgroundColor: useColorModeValue('#ffffff40', '#22222290'),
+            }}
+          >
+            <Link
+              href={`https://www.worldwildlife.org/species/${urlToAnimal}`}
+              isExternal
+              fontFamily={fonts.heading}
+              fontSize={{ base: 'md', md: 'lg' }}
+              _hover={{ textTransform: 'none' }}
+            >
+              Learn more
+            </Link>
+          </Button>
+          <Button
+            w={{ base: '100%', md: '50%' }}
+            backgroundColor="yellowOrange"
+            color="black"
+            _hover={{
+              backgroundColor: '#fec01e80', // yellowOrange
+            }}
+          >
+            <Link
+              href={link_to_adoption}
+              isExternal
+              fontFamily={fonts.heading}
+              fontSize={{ base: 'md', md: 'lg' }}
+              _hover={{ textTransform: 'none' }}
+            >
+              Adopt
+            </Link>
+          </Button>
+        </ButtonGroup>
       </Flex>
-    </Box>
+    </Flex>
   )
 }
 
